4fa0a5030c01d39ac1425dff1a42f811c54b7bbe
Josh Blumberg <jlblumberg@gmail.com>
2020-04-15 11:51:54 +0100

    Play around with different components and props

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css'
 
-function Hi(props) {
-  return <div>Hello {props.name}!</div>;
+const Hi = ({name}) => {
+  return <div>Hello {name}!</div>;
 }
 
-ReactDom.render(<Hi name="Josh"/>, document.querySelector('#root'));
+const MediaCard = ({title, body, imageUrl}) => {
+  return <div>
+            <h2>{title}</h2>
+            <p>{body}</p>
+            <img src={imageUrl}/>
+          </div>
+}
+
+const Gate = ({isOpen}) => {
+  return <div>{isOpen ? "Open" : "Closed"}</div>
+}
+
+// ReactDom.render(<Hi name="Josh"/>, document.querySelector('#root'));
+// ReactDom.render(<MediaCard title="This is the title" body="This is the body" imageUrl="https://i.imgur.com/aoMI4WQ.png"/>, document.querySelector('#root'));
+ReactDom.render(<Gate isOpen={false}/>, document.querySelector('#root'));
