48da8f93532cc7092d2f88c464c523fed9c741b6
Josh Blumberg <jlblumberg@gmail.com>
2020-04-15 11:55:19 +0100

    Set up light switch mini project

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import './index.css'
 
-const Hi = ({name}) => {
-  return <div>Hello {name}!</div>;
+function Room() {
+  return (
+    <div className="room">The room is lit</div>
+  );
 }
 
-const MediaCard = ({title, body, imageUrl}) => {
-  return <div>
-            <h2>{title}</h2>
-            <p>{body}</p>
-            <img src={imageUrl}/>
-          </div>
-}
-
-const Gate = ({isOpen}) => {
-  return <div>{isOpen ? "Open" : "Closed"}</div>
-}
-
-// ReactDom.render(<Hi name="Josh"/>, document.querySelector('#root'));
-// ReactDom.render(<MediaCard title="This is the title" body="This is the body" imageUrl="https://i.imgur.com/aoMI4WQ.png"/>, document.querySelector('#root'));
-ReactDom.render(<Gate isOpen={false}/>, document.querySelector('#root'));
+ReactDom.render(<Room />, document.getElementById('root'));
