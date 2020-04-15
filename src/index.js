1784fd47a1b8402bc7b550ce578474c99f9901ce
Josh Blumberg <jlblumberg@gmail.com>
2020-04-15 13:47:53 +0100

    Add hook and styling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDom from 'react-dom';
+import "./index.css";
 
 function Room() {
+  const state = useState(false);
+  const isLit = state[0];
+  const setLit = state[1];
+  const brightness = isLit ? "lit" : "dark";
+
   return (
-    <div className="room">The room is lit</div>
+    <div className={`room ${brightness}`}>
+      The room is {brightness}
+      <br/>
+      <button onClick={() => setLit(!isLit)}>
+        flip
+      </button>
+    </div>
   );
 }
 
-ReactDom.render(<Room />, document.getElementById('root'));
+ReactDom.render(<Room/>, document.getElementById('root'));
