f974b3a61bea2700e86f96a122fddd403566cd35
Josh Blumberg <jlblumberg@gmail.com>
2020-04-16 09:29:38 +0100

    Add another state hook for temperature

--- src/index.js
+++ src/index.js
@@ -5,17 +5,16 @@ import "./index.css";
 function Room() {
   const [isLit, setLit] = useState(false);
   const brightness = isLit ? "lit" : "dark";
+  const [temp, setTemp] = useState(22)
 
   return (
     <div className={`room ${brightness}`}>
-      The room is {brightness}
-      <br/>
-      <button onClick={() => setLit(true)}>
-        On
-      </button>
-      <button onClick={() => setLit(false)}>
-        Off
-      </button>
+      The room is {brightness} <br />
+      It is {temp} degrees in here <br />
+      <button onClick={() => setLit(true)}>Lights on</button>
+      <button onClick={() => setLit(false)}>Lights off</button><br />
+      <button onClick={() => setTemp(temp + 1)}>Temp +</button>
+      <button onClick={() => setTemp(temp - 1)}>Temp -</button>
     </div>
   );
 }
