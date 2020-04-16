58a042eba7181bad1b8db704493d00c6e5006e6a
Josh Blumberg <jlblumberg@gmail.com>
2020-04-16 12:11:28 +0100

    Add reddit API to pull in title and upvotes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 function Reddit() {
   const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios.get(`https://www.reddit.com/r/reactjs.json`)
+      .then(res => {
+        const newPosts = res.data.data.children
+          .map(obj => obj.data);
+
+        setPosts(newPosts);
+      });
+  }, []);
   return (
     <div>
       <h1>/r/reactjs</h1>
       <ul>
         {posts.map(post => (
-          <li key={post.id}>{post.title}</li>
+          <li key={post.id}>{post.title} | upvotes: {post.ups}</li>
         ))}
       </ul>
     </div>
